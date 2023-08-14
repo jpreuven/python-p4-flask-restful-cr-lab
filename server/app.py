ea3ce79a0dd5ecaf069c182b1543aff3cab83ce7
Jonathan <jpreuven@gmail.com>
2023-08-14 01:28:31 -0700

    COMPLETED

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,10 +17,46 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(
+            jsonify(plants_list),
+            200
+        )
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        new_plant_dict = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price'],
+        )
+        db.session.add(new_plant_dict)
+        db.session.commit()
+
+        response_dict = new_plant_dict.to_dict()
+
+        response = make_response(
+            response_dict,
+            201
+        )
+        return response
+        
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+
+        response = make_response(
+            jsonify(plant_dict),
+            200
+        )
+
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
